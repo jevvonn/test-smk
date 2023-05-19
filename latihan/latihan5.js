bc08179e37d8c4d9eade4d42ab91341002b8cff4
jevvonn <darmawanb985@gmail.com>
2023-05-19 09:58:28 +0700

    latihan 5 done

--- latihan/latihan5.js
+++ latihan/latihan5.js
@@ -1,12 +1,14 @@
 /**
-* Hasilkan nilai faktorial dari n
-* @namespace Latihan
-* @method factorial
-* @param {Number} n number
-* @return {Number} hasil faktorial
-*/
+ * Hasilkan nilai faktorial dari n
+ * @namespace Latihan
+ * @method factorial
+ * @param {Number} n number
+ * @return {Number} hasil faktorial
+ */
 function factorial(n) {
-    // TODO: hasilkan nilai faktorial sebagai return value
-    // apabila n < 0, maka hasilkan nilia undefined
+  // TODO: hasilkan nilai faktorial sebagai return value
+  // apabila n < 0, maka hasilkan nilia undefined
+  if (n < 0) return null;
+  return n == 0 ? 1 : n * factorial(n - 1);
 }
 module.exports = factorial;
